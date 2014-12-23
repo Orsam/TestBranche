3d9c797dfe29ccae7c39f99ddc64ed08c2eb9d54
Orsam <orsam999@msn.com>
2014-12-23 09:22:40 +0100

    Ajout des fonction Soustraction et Multiplication

--- data/Calcul.php
+++ data/Calcul.php
@@ -6,5 +6,10 @@ class Calcul {
     public static function Division($a,$b) {
         return $a/$b;
     }
-    
+    public static function Soustraction($a,$b) {
+        return $a-$b;
+    }
+    public static function Multiplication($a,$b) {
+        return $a*$b;
+    }
 }
